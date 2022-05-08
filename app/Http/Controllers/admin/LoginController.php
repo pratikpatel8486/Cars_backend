c82f76c8b6083ba6c66cbc4002c8e164112f991c
payal <payal@honeycombsoftwares.com>
2022-05-08 22:27:19 +0530

    admin login api

--- app/Http/Controllers/admin/LoginController.php
+++ app/Http/Controllers/admin/LoginController.php
@@ -11,6 +11,51 @@ use Illuminate\Support\Facades\URL;
 
 class LoginController extends Controller
 {
+    // public function AdminLogin(Request $request)
+    // {
+    //     $requestData = $request->json()->all();
+    //     if (count($requestData) > 0) {
+    //         if (!empty($requestData['email']) && !empty($requestData['password'])) {
+    //             $email = $requestData['email'];
+    //             $password = $requestData['password'];
+    //         } else {
+    //             return $this->sendError(Lang::get("auth.require_email_password", array(), $this->selected_language), json_decode("{}"), 201);
+    //         }
+    //        $hasher = app()->make('hash');
+    //        dd($hasher);
+    //         $login = User::where('email', $email)->first();
+    //         if (!$login) {
+    //             return $this->sendError(Lang::get("auth.invalid_login", array(), $this->selected_language), json_decode("{}"), 201);
+    //         } else {
+    //             if ($login) {
+    //                 if (!empty($login->deleted_at)) {
+    //                     return $this->sendError(Lang::get("auth.account_deactivated", array(), $this->selected_language), json_decode("{}"), 201);
+    //                 }
+    //             }
+    //             if ($hasher->check($password, $login->password)) {
+    //                 if (!$create_token = Auth::attempt($requestData)) {
+    //                     return $this->sendError(Lang::get("common.unauthorized", array(), $this->selected_language), json_decode("{}"), 401);
+    //                 }
+    //                 // Update device token
+    //                 User::where('id', $login->id)->update(['api_token' => $create_token]);
+    //                 if ($create_token) {
+    //                     $User = $login;
+    //                     $tokens = $this->respondWithToken($create_token);
+    //                     $data['api_token'] = $tokens['api_token'];
+    //                     $data['token_expires_in'] = $tokens['token_expires_in'];
+    //                     $data['user_info'] = $User;
+    //                     return $this->sendResponse($data, Lang::get("common.success", array(), $this->selected_language));
+    //                 }
+    //             } else {
+    //                 return $this->sendError(Lang::get("auth.invalid_login", array(), $this->selected_language), json_decode("{}"), 201);
+    //             }
+    //         }
+    //     } else {
+    //         return $this->sendError(Lang::get("common.request_invalid", array(), $this->selected_language), json_decode("{}"), 400);
+    //     }
+    // }
+
+
     public function AdminLogin(Request $request)
     {
         $requestData = $request->json()->all();
@@ -19,38 +64,28 @@ class LoginController extends Controller
                 $email = $requestData['email'];
                 $password = $requestData['password'];
             } else {
-                return $this->sendError(Lang::get("auth.require_email_password", array(), $this->selected_language), json_decode("{}"), 201);
+                return $this->sendError(Lang::get("auth.require_email_password", array(), ''), json_decode("{}"), 201);
             }
-            $hasher = app()->make('hash');
+         //  $hasher = app()->make('hash');
+          // dd($hasher);
             $login = User::where('email', $email)->first();
             if (!$login) {
-                return $this->sendError(Lang::get("auth.invalid_login", array(), $this->selected_language), json_decode("{}"), 201);
+                return response()->json(['success' => 'false', 'messae' => 'invalid_login']);
+              //  return $this->sendError(Lang::get("auth.invalid_login", array(), ''), json_decode("{}"), 201);
             } else {
-                if ($login) {
-                    if (!empty($login->deleted_at)) {
-                        return $this->sendError(Lang::get("auth.account_deactivated", array(), $this->selected_language), json_decode("{}"), 201);
-                    }
-                }
-                if ($hasher->check($password, $login->password)) {
-                    if (!$create_token = Auth::attempt($requestData)) {
-                        return $this->sendError(Lang::get("common.unauthorized", array(), $this->selected_language), json_decode("{}"), 401);
-                    }
-                    // Update device token
-                    User::where('id', $login->id)->update(['api_token' => $create_token]);
-                    if ($create_token) {
-                        $User = $login;
-                        $tokens = $this->respondWithToken($create_token);
-                        $data['api_token'] = $tokens['api_token'];
-                        $data['token_expires_in'] = $tokens['token_expires_in'];
-                        $data['user_info'] = $User;
-                        return $this->sendResponse($data, Lang::get("common.success", array(), $this->selected_language));
-                    }
-                } else {
-                    return $this->sendError(Lang::get("auth.invalid_login", array(), $this->selected_language), json_decode("{}"), 201);
-                }
+               // CHECK IF PAS AND ID ARE VALID
+               $login_true = User::where('email', $email)->where('password', $password)->first();
+
+               if($login_true){
+                return response()->json(['success' => 'true', 'messae' => 'Login successfully']);
+               }else{
+                return response()->json(['success' => 'false', 'messae' => 'invalid_login']);
+               }
+             
             }
         } else {
-            return $this->sendError(Lang::get("common.request_invalid", array(), $this->selected_language), json_decode("{}"), 400);
+            return response()->json(['success' => 'false', 'messae' => 'Request invalid']);
+           // return $this->sendError(Lang::get("common.request_invalid", array(), ''), json_decode("{}"), 400);
         }
     }
 }
